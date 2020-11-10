ba134c84850a7e12140cb32ff64195dc812c99bb
Dexter Quiambao <dexterquiambao@googlemail.com>
2020-11-10 16:01:17 +0000

    Working but not styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # READ ALL TASKS
-  get "tasks", to: "tasks#index"
-  # CREATE A TASK
-  get "tasks/new", to: "tasks#new", as: "new_task"
-   # READ ONE TASKS
-  get "tasks/:id", to: "tasks#show", as: "task"
-  # CREATE A TASK
-  post "tasks", to: "tasks#create"
-  # UPDATE A TASK
-  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
-  patch "tasks/:id", to: "tasks#update"
-  #  DELETE A TASK
-  delete 'tasks/:id', to: 'tasks#destroy'
+  get "/tasks", to: "tasks#index"
+
+  post "/tasks", to: "tasks#create"
+  get "/tasks/new", to: "tasks#new", as: :new_task
+
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "/tasks/:id/", to: "tasks#update"
+
+  delete "/tasks/:id", to: "tasks#destroy"
+
+  get "/tasks/:id", to:"tasks#show", as: :task
 
   # resources :tasks
 
